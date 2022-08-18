e02030fa7c32e9b5b0ab3f1d483e76a96f64292a
Raghav <raghavarya2002@gmail.com>
2022-08-18 14:20:20 +0530

    Update

--- FileCompressionHuffmanCoding.py
+++ FileCompressionHuffmanCoding.py
@@ -1,124 +1,101 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "FileCompressionHuffmanCoding.ipynb",
-      "provenance": [],
-      "collapsed_sections": [],
-      "authorship_tag": "ABX9TyO9Y6sNxoNtxFg0Q5mlXW4s",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/Raghavarya2002/File-Compression-Huffman-Coding/blob/main/FileCompressionHuffmanCoding.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "execution_count": null,
-      "metadata": {
-        "id": "SYveutrWHE7o"
-      },
-      "outputs": [],
-      "source": [
-        "import heapq #default inbuild heap\n",
-        "\n",
-        "class BinaryTree:\n",
-        "  def __init__(self,value,freq):\n",
-        "    self.value = value\n",
-        "    self.freq = freq\n",
-        "    self.left = None\n",
-        "    self.right = None\n",
-        "\n",
-        "class Huffmancode:\n",
-        "\n",
-        "  def __init__(self,path) #path of the file\n",
-        "      self.path = path\n",
-        "      self.__heap = []\n",
-        "\n",
-        "  def __leftfunc__(self,other):\n",
-        "    return self.freq < other.freq #checking & comparing on frequency and elemnet have min freq we're picking up that\n",
-        "  \n",
-        "  def __equalfunc__(self,other):\n",
-        "    return self.freq == other.freq\n",
-        "\n",
-        "  def __frequency_from_text(self,text):\n",
-        "      freq_dict = {}\n",
-        "      for char in text:\n",
-        "        if char not in freq_dict:\n",
-        "          freq_dict[char] = 0\n",
-        "        freq_dict[char] += 1\n",
-        "      return freq_dict  \n",
-        "\n",
-        "\n",
-        "\n",
-        "\n",
-        "  def __Build_heap(self,frequency_dict):\n",
-        "      for key in frequency_dict\n",
-        "\n",
-        "      frequency = frequency_dict[key]\n",
-        "      binary_tree_node = BinaryTree(key,frequency)\n",
-        "      heapq.push(self.__heap,binary_tree_node)\n",
-        "\n",
-        "  def __Build_Binary_Tree(self):\n",
-        "    while(self.__heap) > 1:\n",
-        "    binaryTree_Node_1 = heapq.heappop(self.__heap)\n",
-        "    binaryTree_Node_2 = heapq.heappop(self.__heap)\n",
-        "    sum_of_freq = binaryTree_Node_1.freq + binaryTree_Node_2.freq\n",
-        "    newNode = BinaryTree(None,sum_of_freq)\n",
-        "\n",
-        "    newNode.left = binaryTree_Node_1\n",
-        "    newNode.right = binaryTree_Node_2\n",
-        "    return\n",
-        "\n",
-        "\n",
-        "  def __Build_Tree_Code(self.__heap):\n",
-        "    root = heapq.heappop(self.__heap)\n",
-        "\n",
-        "\n",
-        "  def compression(self):\n",
-        "\n",
-        "    #To access the file and extarct text from that file\n",
-        "\n",
-        "\n",
-        "    text = 'fbvjbvjbjcjdjsdnjdsjdsjsfjn'\n",
-        "    frequency_dict = self.__frequency_from_text(text)\n",
-        "\n",
-        "    #calculate frequency of each text and store in the frequency dicionary\n",
-        "\n",
-        "    build_heap = self.__Build_heap(frequency_dict)\n",
-        "\n",
-        "\n",
-        "    #we need minimum 2characters having minimum frequency , so we have to construct a min heap\n",
-        "    \n",
-        "    #Min heap for two minimum frequency\n",
-        "    #so after this we've to create the binary tree using the heap\n",
-        "\n",
-        "  self.__Build_Binary_Tree()\n",
-        "\n",
-        "    #with the hellp of binary tree , we've to construct the code and code will store in the heap or you can say hashmap or dictionary\n",
-        "\n",
-        "    self.__Build_Tree_Code()\n",
-        "\n",
-        "\n",
-        "    #construct the encoded text.\n",
-        "    #we have to return that binary file as output containing encoded text which is given by compression function"
-      ]
-    }
-  ]
-}
+import heapq #default inbuild heap
+
+class BinaryTree:
+  def __init__(self,value,freq):
+    self.value = value
+    self.freq = freq
+    self.left = None
+    self.right = None
+
+class Huffmancode:
+
+  def __init__(self,path) #path of the file
+      self.path = path
+      self.__heap = []
+      self.__code = {}
+
+  def __leftfunc__(self,other):
+    return self.freq < other.freq #checking & comparing on frequency and elemnet have min freq we're picking up that
+  
+  def __equalfunc__(self,other):
+    return self.freq == other.freq
+
+  def __frequency_from_text(self,text):
+      freq_dict = {}
+      for char in text:
+        if char not in freq_dict:
+          freq_dict[char] = 0
+        freq_dict[char] += 1
+      return freq_dict  
+
+
+
+
+  def __Build_heap(self,frequency_dict):
+      for key in frequency_dict
+
+      frequency = frequency_dict[key]
+      binary_tree_node = BinaryTree(key,frequency)
+      heapq.push(self.__heap,binary_tree_node)
+
+  def __Build_Binary_Tree(self):
+    while(self.__heap) > 1:
+    binaryTree_Node_1 = heapq.heappop(self.__heap)
+    binaryTree_Node_2 = heapq.heappop(self.__heap)
+    sum_of_freq = binaryTree_Node_1.freq + binaryTree_Node_2.freq
+    newNode = BinaryTree(None,sum_of_freq)
+
+    newNode.left = binaryTree_Node_1
+    newNode.right = binaryTree_Node_2
+    return
+
+  def __Build_Tree_Code_Helper(self,root,curr_bits):
+    if root is None:
+      return
+      if root.value is not None:
+        self.__code[root.value] = curr_bits
+        return
+      self.__Build_Tree_Code_Helper(root.left,curr_bits+'0')
+      self.__Build_Tree_Code_Helper(root.rigth,curr_bits+'1')
+
+  def __Build_Tree_Code(self.__heap):
+    root = heapq.heappop(self.__heap)
+    self.__Build_Tree_Code_Helper(root,'')
+
+
+  def __Build_Encoded_Text(self,text):
+    encoded_text = ''
+    for char in text:
+      encoded_text += self.__code[char]
+
+    return encoded_text
+
+
+  def compression(self):
+
+    #To access the file and extarct text from that file
+
+
+    text = 'fbvjbvjbjcjdjsdnjdsjdsjsfjn'
+    frequency_dict = self.__frequency_from_text(text)
+
+    #calculate frequency of each text and store in the frequency dicionary
+
+    build_heap = self.__Build_heap(frequency_dict)
+
+
+    #we need minimum 2characters having minimum frequency , so we have to construct a min heap
+    
+    #Min heap for two minimum frequency
+    #so after this we've to create the binary tree using the heap
+
+  self.__Build_Binary_Tree()
+
+    #with the hellp of binary tree , we've to construct the code and code will store in the heap or you can say hashmap or dictionary
+
+    self.__Build_Tree_Code()
+
+
+    #construct the encoded text.
+    encoded_text = self.__Build_Encoded_Text(text)
+    #we have to return that binary file as output containing encoded text which is given by compression function
